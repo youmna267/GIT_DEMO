56e60072bce28b3a0cadcf2634a5912294b6dde1
youmna267 <darkverse67@gmail.com>
2026-05-23 01:48:42 -0400

    its my first change

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+
+
 # GIT_DEMO
+this is my second change
+fgtrhg6thjyj
+this is my third change
+this is my fourth change
---
+++ app.py
@@ -0,0 +1,4 @@
+print("===== Simple Calculator =====")
+print("1. Addition")
+print("2. Multiplication")
+print("3. Exit")
